60daae462d70e3d496bacb3133266bae5e8851f1
GANESH TELORE <ganeshtelore4@gmail.com>
2025-09-23 00:27:39 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,7 +1,7 @@
-//from pymongo import MongoClient
+'''from pymongo import MongoClient
 
-//MONGO_URI = "mongodb://localhost:27017/"
-//MONGO_DB_NAME = "aura"
+MONGO_URI = "mongodb://localhost:27017/"
+MONGO_DB_NAME = "aura"
 
-//mongo_client = MongoClient(MONGO_URI)
-//mongo_db = mongo_client[MONGO_DB_NAME]
+mongo_client = MongoClient(MONGO_URI)
+mongo_db = mongo_client[MONGO_DB_NAME]'''
