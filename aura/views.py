5af0f63ac0c8577476b527ec0cab8a8094f23cba
GANESH TELORE <ganeshtelore4@gmail.com>
2025-09-23 00:36:17 +0530

    Update views.py

--- aura/views.py
+++ aura/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect 
-from models.herbs import herbs
+#from models.herbs import herbs
 
 def home(request):
-    data=herbs()
-    return render(request, "index.html", {"herbs":data})
+    #data=herbs()
+    return render(request, "index.html")
 
 '''def herb(request):
     return render(request, "herbs.html")'''
